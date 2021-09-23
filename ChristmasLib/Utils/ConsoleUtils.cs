35c6cb103946145b209f75ed245c37110d3b1f7e
notpikmin <notpikmin@gmail.com>
2021-09-23 14:12:02 -0700

    Added Console Utils

---
+++ ChristmasLib/Utils/ConsoleUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChristmasLib.Utils
+{
+    public static class ConsoleUtils
+    {
+        public static void Write(string Input, string mod = "Christmas")
+        {
+            Console.ResetColor();
+            string time = "[" + DateTime.Now.ToString("HH:mm:ss") + "]";
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(time);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(" [" + mod + "]: ");
+            Console.ResetColor();
+            Console.Write(Input + "\n");
+            //Console.WriteLine(time + " [" + mod +"]: "+ Input);
+        }
+    }
+}
