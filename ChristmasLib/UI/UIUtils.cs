986122049f04654ed3723a8107d5a0b4c1ecfe8a
notpikmin <notpikmin@gmail.com>
2021-09-24 20:48:07 -0700

    Started  started on ui

---
+++ ChristmasLib/UI/UIUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ChristmasLib.UI
+{
+    public static class UIUtils
+    {
+        public static GameObject CanvasObject;
+        public static void UIStart(string id)
+        {
+            CanvasObject = new GameObject();
+            CanvasObject.name = id;   
+            UnityEngine.Object.DontDestroyOnLoad(CanvasObject);
+
+            Canvas canvas = CanvasObject.AddComponent<Canvas>();
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+
+
+
+
+        }
+
+
+
+    }
+}
