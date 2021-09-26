61d7c8a730ce3484d8d0b05a22bbbbc74a83332c
notpikmin <notpikmin@gmail.com>
2021-09-25 19:43:50 -0700

    UI stuff

---
+++ ChristmasLib/UI/ButtonGroup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ChristmasLib.UI
+{
+    public class ButtonGroup
+    {
+
+        public List<UIButton> Buttons;
+        public float panelHeight, x,y,ButtonGap,ButtonBorder;
+        public string Name;
+        public Color BGColor;
+        ButtonGroup(string name, List<UIButton> buttons, float bWidth,float bHeight, float buttonGap,float buttonBorder,Color BGColor, float xp = 0, float yp = 0)
+        {
+            this.Buttons = buttons;
+            this.panelHeight = (bHeight * buttons.Count) + (buttonGap * buttons.Count);
+            this.x = xp;
+            this.y = yp;
+            this.ButtonGap = buttonGap;
+            this.ButtonBorder = buttonBorder;
+            this.Name = name;
+
+            initButtonGroup(this);
+
+        }
+
+        private void initButtonGroup(ButtonGroup b)
+        {
+            GameObject BtnGrpObj = new GameObject();
+            BtnGrpObj.name = b.Name;
+            RectTransform rect= BtnGrpObj.AddComponent<RectTransform>();
+            rect.sizeDelta = new Vector2( b.panelHeight + b.ButtonBorder,b.panelHeight);
+            rect.anchoredPosition = new Vector2(b.x, b.y);
+            rect.anchorMin = new Vector2(0.5f, 1);
+            rect.anchorMax = new Vector2(0.5f, 1);
+            rect.pivot = new Vector2(0.5f, 0.5f);
+
+            Image panel = BtnGrpObj.AddComponent<Image>();
+            panel.sprite = null;
+            panel.color = b.BGColor;
+
+
+            foreach (UIButton uiB in b.Buttons)
+            {
+                GameObject btn = UIButton.createButton(uiB);
+                btn.transform.parent = BtnGrpObj.transform;
+            }
+
+
+        }
+
+
+    }
+}
---
+++ ChristmasLib/UI/UIButton.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+namespace ChristmasLib.UI
+{
+    public class UIButton
+    {
+
+
+        public string Text;
+        public float Width, Height,x,y;
+        public UnityAction OnClick;
+        public Color BGColor,TextColor;
+        public GameObject gameObject;
+
+        UIButton(string text, float width, float height, UnityAction onClick, Color bgColor, Color textcolor, float xp = 0,float yp =0)
+        {
+            this.Text = text;
+            this.Width = width;
+            this.Height = height;
+            this.OnClick = onClick;
+            this.x = xp;
+            this.y = yp;
+            this.BGColor = bgColor;
+            this.TextColor = textcolor;
+
+        }
+
+        public static GameObject createButton(UIButton btn)
+        {
+            GameObject ButtonObject = new GameObject();
+            ButtonObject.name = "Button" + btn.Text;
+
+            RectTransform btnran = ButtonObject.AddComponent<RectTransform>();
+            btnran.anchoredPosition = btn.x;                                                                                                                                                                                
+
+            Button Button = ButtonObject.AddComponent<Button>();
+            Button.onClick.AddListener(btn.OnClick);
+
+            Image BtnBG = ButtonObject.AddComponent<Image>();
+            BtnBG.sprite = null;
+            BtnBG.color = btn.BGColor;
+
+            GameObject TextObject = new GameObject();
+            TextObject.name = "Text";
+            TextObject.transform.parent = ButtonObject.transform;
+
+            Text BtnText = TextObject.AddComponent<Text>();
+            BtnText.resizeTextForBestFit = true;
+            BtnText.alignment = TextAnchor.MiddleCenter;
+            BtnText.text = btn.Text;
+            BtnText.color = btn.TextColor;
+
+            return ButtonObject;
+        }
+        
+
+    }
+}
