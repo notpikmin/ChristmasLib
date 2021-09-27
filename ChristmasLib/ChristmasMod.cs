b787c8e606e5e2de3e490ec312b4385120defd8b
notpikmin <notpikmin@gmail.com>
2021-09-27 15:28:29 -0700

    Added Christmas Mod

---
+++ ChristmasLib/ChristmasMod.cs
@@ -0,0 +1,47 @@
+﻿using MelonLoader;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChristmasLib
+{
+    public abstract class ChristmasMod :MelonMod
+    {
+        public virtual void OnSceneWasLoaded(int buildIndex, string sceneName) { }
+
+        public virtual void OnSceneWasInitialized(int buildIndex, string sceneName) { }
+
+        public virtual void OnSceneWasUnloaded(int buildIndex,string sceneName) { }
+
+        public virtual void OnFixedUpdate() { }
+
+        public virtual void OnUIInit() { }
+        public virtual void OnStart() { }
+
+
+        private  IEnumerator CheckForUi()
+        {
+            while (VRCUiManager.prop_VRCUiManager_0 == null)
+            {
+                yield return null;
+
+            }
+            while (QuickMenu.prop_QuickMenu_0 == null)
+            {
+                yield return null;
+            }
+            yield return null;
+            OnUIInit();
+        }
+        public override void OnApplicationStart() 
+        {
+            OnStart();
+            MelonCoroutines.Start(CheckForUi());
+
+        }
+
+    }
+}
---
+++ ChristmasLib/Main.cs
@@ -0,0 +1,25 @@
+﻿using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+[assembly: MelonInfo(typeof(ChristmasLib.Main), "ChristmasLib", "1.0.0", "Pikmin", "http://www.goorlandostore.com")]
+[assembly: MelonGame("VRChat", "VRChat")]
+
+namespace ChristmasLib
+{
+    public class Main :ChristmasMod
+    {
+        public override void OnStart()
+        {
+            
+        }
+        public override void OnUIInit()
+        {
+            
+        }
+
+
+    }
+}
