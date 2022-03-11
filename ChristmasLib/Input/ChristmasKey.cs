a32d10888fe839dc51ff1dc8198fc27a05d2a61f
notpikmin <notpikmin@gmail.com>
2022-03-10 19:54:33 -0800

    Updated ChristmasKey.cs

--- ChristmasLib/Input/ChristmasKey.cs
+++ ChristmasLib/Input/ChristmasKey.cs
@@ -7,13 +7,25 @@ namespace ChristmasLib.Input
     {
         public string Key;
         public bool UseCtrl;
-        public KeyCode Keycode;
+        //public KeyCode Keycode;
+        public ChristmasKey(KeyCode key, bool useCtrl)
+        {
+            Key = key.ToString();
+            UseCtrl = useCtrl;
+            //Keycode = ConfigUtils.ParseKeyCode(key);
+        }
+
         public ChristmasKey(string key, bool useCtrl)
         {
             Key = key;
             UseCtrl = useCtrl;
-            Keycode = ConfigUtils.ParseKeyCode(key);
+            //Keycode = ConfigUtils.ParseKeyCode(key);
         }
-        
+     
+
+        public KeyCode GetKeyCode()
+        {
+            return ConfigUtils.ParseKeyCode(this.Key);
+        }
     }
 }
