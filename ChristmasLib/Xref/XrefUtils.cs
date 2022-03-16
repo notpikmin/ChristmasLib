d65284b67d9893f5f6e46452f7e60c42281977b8
notpikmin <notpikmin@gmail.com>
2022-03-16 12:57:40 -0700

    Implemented Xref

---
+++ ChristmasLib/Xref/XrefUtils.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Reflection;
+using UnhollowerRuntimeLib.XrefScans;
+
+namespace ChristmasLib.Xref
+{
+    public static class XrefUtils
+    {
+            
+
+        private static bool XRefScanForMethod(this MethodBase methodBase, string methodName = null, string reflectedType = null)
+        {
+            foreach (XrefInstance instance in XrefScanner.XrefScan(methodBase))
+            {
+                var found = false;
+                if (instance.Type != XrefType.Method) continue;
+
+                MethodBase resolved = instance.TryResolve();
+                if (resolved == null) continue;
+
+                if (!string.IsNullOrEmpty(methodName))
+                    found = !string.IsNullOrEmpty(resolved.Name) && resolved.Name.IndexOf(methodName, StringComparison.OrdinalIgnoreCase) >= 0;
+
+                if (!string.IsNullOrEmpty(reflectedType))
+                    found = !string.IsNullOrEmpty(resolved.ReflectedType?.Name) && resolved.ReflectedType.Name.IndexOf(
+                                reflectedType,
+                                StringComparison.OrdinalIgnoreCase) >= 0;
+
+                if (found) return true;
+            }
+
+            return false;
+        }
+
+    }
+}
