f093556a578cd7e10da0dd6121c66627b05ed43e
notpikmin <notpikmin@gmail.com>
2022-04-23 23:52:31 -0700

    AvatarLoad patch added summaries to defined patches containing params

--- ChristmasLib/Patches/PatchDefine.cs
+++ ChristmasLib/Patches/PatchDefine.cs
@@ -1,5 +1,4 @@
-﻿using ExitGames.Client.Photon;
-using Photon.Pun;
+﻿using Photon.Pun;
 using Photon.Realtime;
 using System.Linq;
 using System.Reflection;
@@ -15,37 +14,70 @@ namespace ChristmasLib.Patches
 
         #region Photon
         
-        public static MethodInfo LoadBalanceRaiseEvent = typeof(LoadBalancingClient).GetMethod("Method_Public_Virtual_New_Boolean_Byte_Object_RaiseEventOptions_SendOptions_0");
-        public static MethodInfo LoadBalanceEvent = typeof(LoadBalancingClient).GetMethod("OnEvent");
+        /// <summary>
+        /// bool LoadBalanceRaiseEvent(byte eventCode, Object data, RaiseEventOptions raiseEventOptions, SendOptions sendOptions)
+        /// </summary>
+        public static readonly MethodInfo LoadBalanceRaiseEvent = typeof(LoadBalancingClient).GetMethod(
+            nameof(LoadBalancingClient.Method_Public_Virtual_New_Boolean_Byte_Object_RaiseEventOptions_SendOptions_0));
+       
+        /// <summary>
+        /// void LoadBalanceEvent(EventData eventData)
+        /// </summary>
+        public static readonly MethodInfo LoadBalanceEvent = typeof(LoadBalancingClient).GetMethod( nameof(LoadBalancingClient.OnEvent));
 
-        public static MethodInfo PhotonRaiseEvent = typeof(PhotonNetwork).GetMethod("Method_Public_Static_Boolean_Byte_Object_RaiseEventOptions_SendOptions_0");
-        public static MethodInfo PhotonEvent = typeof(PhotonNetwork).GetMethod("Method_Private_Static_Void_EventData_PDM_0");
+        /// <summary>
+        /// bool PhotonRaiseEvent(byte eventCode, Object data, RaiseEventOptions raiseEventOptions, SendOptions sendOptions)
+        /// </summary>
+        public static readonly MethodInfo PhotonRaiseEvent = typeof(PhotonNetwork).GetMethod(
+            nameof(PhotonNetwork.Method_Public_Static_Boolean_Byte_Object_RaiseEventOptions_SendOptions_0));
+        
+        /// <summary>
+        /// void PhotonEvent(EventData eventData)
+        /// </summary>
+        public static readonly MethodInfo PhotonEvent = typeof(PhotonNetwork).GetMethod(
+            nameof(PhotonNetwork.Method_Private_Static_Void_EventData_PDM_0));
 
-        public static MethodInfo EnetEnqueue = typeof(EnetPeer).GetMethod("EnqueueOperation");
+        //not valid anymore
+        //public static MethodInfo EnetEnqueue = typeof(EnetPeer).GetMethod("EnqueueOperation");
 
         #endregion
         
         #region Event
-        public static MethodInfo InternalTrigger = typeof(VRC_EventHandler).GetMethod(nameof(VRC_EventHandler.InternalTriggerEvent));
+        
+        /// <summary>
+        /// void InternalTriggerEvent(VrcEvent e, VrcBroadcastType broadcastType, int instigatorId, float fastForward)
+        /// </summary>
+        public static readonly MethodInfo InternalTrigger = typeof(VRC_EventHandler).GetMethod(nameof(VRC_EventHandler.InternalTriggerEvent));
 
+        /// <summary>
+        /// void EmojiEvent(Player playerSending, VrcEvent event, VrcBroadcastType broadcastType, int instigatorId, float fastForward)
+        /// </summary>
+        public static readonly MethodInfo EmojiEvent = typeof(VRC_EventDispatcherRFC).GetMethod(
+            nameof(VRC_EventDispatcherRFC.Method_Public_Void_Player_VrcEvent_VrcBroadcastType_Int32_Single_0));
 
-        public static MethodInfo EmojiEvent = typeof(VRC_EventDispatcherRFC).GetMethod("Method_Public_Void_Player_VrcEvent_VrcBroadcastType_Int32_Single_0");
-
-        public static MethodInfo Emoji = typeof(VRCPlayer).GetMethod(nameof(VRCPlayer.SpawnEmojiRPC));
+        /// <summary>
+        /// void SpawnEmojiRPC(int __0, Player __1)
+        /// </summary>
+        public static readonly MethodInfo Emoji = typeof(VRCPlayer).GetMethod(nameof(VRCPlayer.SpawnEmojiRPC));
 
         #endregion
 
         #region JoinAndLeave
-        
-        public static MethodInfo LocalLeave = typeof(NetworkManager).GetMethod(nameof(NetworkManager.OnJoinedRoom));
-        public static MethodInfo LocalJoin = typeof(NetworkManager).GetMethod(nameof(NetworkManager.OnJoinedRoom));
+        /// <summary>
+        /// void OnLeftRoom()
+        /// </summary>
+        public static readonly MethodInfo LocalLeave = typeof(NetworkManager).GetMethod(nameof(NetworkManager.OnLeftRoom));
+        /// <summary>
+        /// void OnJoinedRoom()
+        /// </summary>
+        public static readonly MethodInfo LocalJoin = typeof(NetworkManager).GetMethod(nameof(NetworkManager.OnJoinedRoom));
 
         
         //from Chromatic api 
         //JoinAndLeave[1] = join
         //JoinAndLeave[0] = leave
         //changes often :/
-        public static MethodInfo[] JoinAndLeave = typeof(NetworkManager).GetMethods().Where(m =>
+        public static readonly MethodInfo[] JoinAndLeave = typeof(NetworkManager).GetMethods().Where(m =>
          m.Name.Contains("Method_Public_Void_Player_") &&
          !m.Name.Contains("PDM")
         ).ToArray();
@@ -54,39 +86,52 @@ namespace ChristmasLib.Patches
         #region Avatar
         
         //May break in future
+        /*
         public static MethodInfo[] CalculatePerformance = typeof(AvatarPerformance).GetMethods().Where(m =>
         m.Name.Contains("_AvatarPerformanceStats_") &&
         m.Name.StartsWith("Method") && 
         m.Name.Contains("String")
-       ).ToArray();
+        ).ToArray();
+        */      
+        
+        /// <summary>
+        /// bool AvatarLoad(ApiAvatar __0, GameObject __1)
+        /// </summary>
+        public static readonly MethodInfo AvatarLoad = typeof(VRCAvatarManager).GetMethod(nameof(VRCAvatarManager.Method_Private_Boolean_ApiAvatar_GameObject_0));
 
+        /// <summary>
+        /// void RunPerformanceScan(GameObject avatarObject, AvatarPerformanceStats perfStats)
+        /// </summary>
+        public static readonly MethodInfo CalculatePerformance = typeof(PerformanceScannerSet).GetMethod(nameof(PerformanceScannerSet.RunPerformanceScan));
 
         //believe is outdated
         public static MethodInfo SwitchAvatarMethod
         {
             get
             {
-                if (Switchavamethod != null) return Switchavamethod;
-                MethodInfo[] methods = typeof(VRCPlayer).GetMethods(BindingFlags.Instance | BindingFlags.Public);
+                if (SwitchAvatar != null) return SwitchAvatar;
+                var methods = typeof(VRCPlayer).GetMethods(BindingFlags.Instance | BindingFlags.Public);
                 foreach (MethodInfo methodInfo in methods)
                 {
                     //only continue if the method is private, returns void, the parameter length is 1 and its bool with a default value
                     if (!methodInfo.Name.Contains("Private") || methodInfo.ReturnType != typeof(void) || methodInfo.GetParameters().Length != 1 ||
                         methodInfo.GetParameters()[0].ParameterType != typeof(bool) || !methodInfo.GetParameters()[0].HasDefaultValue) continue;
-                    Switchavamethod = methodInfo;
-                    return Switchavamethod;
+                    SwitchAvatar = methodInfo;
+                    return SwitchAvatar;
                 }
-                return Switchavamethod;
+                return SwitchAvatar;
             }
         }
 
-        public static MethodInfo Switchavamethod;
+        public static MethodInfo SwitchAvatar;
         
         #endregion
 
         #region Menu
-
-        public static MethodInfo MenuOpen = typeof(MenuStateController).GetMethod("Method_Private_Void_0");
+        /// <summary>
+        /// void MenuOpen()
+        /// </summary>
+        public static readonly MethodInfo MenuOpen = typeof(MenuStateController).GetMethod(nameof(MenuStateController.Method_Private_Void_0));
         
         #endregion
     }
